230dbe9c5faf30bc8d30d99665dadccc81c426e5
Issam <Issam.aboulfadl05@gmail.com>
2022-02-08 00:25:27 +0100

    add home page css �

--- pages/home.js
+++ pages/home.js
@@ -6,12 +6,12 @@ import {
 	Group,
 	OndemandVideoSharp,
 	BusinessCenter,
-	// ArrowForwardIosOutlined,
+	ArrowForwardIosOutlined,
 } from "@mui/icons-material";
 
 const home = () => {
 	return (
-		<div>
+		<div className="space-y-10 relative">
 			<Head>
 				<title>LinkedIn</title>
 				<link rel="icon" href="/favicon.ico" />
@@ -39,6 +39,35 @@ const home = () => {
 					</div>
 				</div>
 			</header>
+			<main className="flex flex-col xl:flex-row items-center max-w-screen-lg mx-auto">
+				<div className="space-y-6 xl:space-y-10">
+					<h1 className="text-3xl md:text-5xl text-amber-800/80 max-w-xl !leading-snug pl-4 xl:pl-0">
+						Welcome to your professional community
+					</h1>
+					<div className="space-y-4">
+						<div className="intent">
+							<h2 className="text-xl">Search</h2>
+							<ArrowForwardIosOutlined className="text-gray-700" />
+						</div>
+						<div className="intent">
+							<h2 className="text-xl">Find a person you know </h2>
+							<ArrowForwardIosOutlined className="text-gray-700" />
+						</div>
+						<div className="intent">
+							<h2 className="text-xl">Learn a new skill</h2>
+							<ArrowForwardIosOutlined className="text-gray-700" />
+						</div>
+					</div>
+				</div>
+				<div className="relative xl:absolute w-80 h-100 xl:w-[650px] xl:h-[650px] right-5 top-4">
+					<Image
+						src="https://rb.gy/vkzpzt"
+						layout="fill"
+						priority
+						alt="ImageHome"
+					/>
+				</div>
+			</main>
 		</div>
 	);
 };
