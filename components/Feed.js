0d50961c15b88ac55710e3102185f4dc40ff36b0
Issam <Issam.aboulfadl05@gmail.com>
2022-02-24 20:52:37 +0100

    💱Creating atom for posts variables

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useEffect } from "react";
 import Input from "./Input";
+import { handlePostState, useSSRPostsState } from "../atoms/postAtom";
 
 const Feed = () => {
+  const [realtimePosts, setRealtimePosts] = useState([]);
+  const [handlePost, setHandlePost] = useRecoilState(handlePostState);
+
+  useEffect(() => {
+    const fetPosts = async () => {
+      const response = await fetch("/api/posts", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      const responseData = await response.json();
+      setRealtimePosts(responseData);
+    };
+    fetPosts();
+  }, []);
+
   return (
     <div className="space-y-6 pb-24 max-w-lg">
       <Input />
