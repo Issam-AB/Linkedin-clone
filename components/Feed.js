9d13867c2f19324a5e298449262bf749176d02ae
Issam <Issam.aboulfadl05@gmail.com>
2022-02-25 00:49:29 +0100

    💯 Using SSR and re-fetching

--- components/Feed.js
+++ components/Feed.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import Input from "./Input";
 import { handlePostState, useSSRPostsState } from "../atoms/postAtom";
+import { useRecoilState } from "recoil";
+import Post from "./Post";
 
 const Feed = () => {
   const [realtimePosts, setRealtimePosts] = useState([]);
   const [handlePost, setHandlePost] = useRecoilState(handlePostState);
+  const [useSSRPosts, setUseSSRPosts] = useRecoilState(useSSRPostsState);
 
   useEffect(() => {
     const fetPosts = async () => {
@@ -14,7 +17,10 @@ const Feed = () => {
         headers: { "Content-Type": "application/json" },
       });
       const responseData = await response.json();
+
       setRealtimePosts(responseData);
+      setHandlePost(false);
+      setUseSSRPosts(false);
     };
     fetPosts();
   }, [handlePost]);
@@ -22,7 +28,14 @@ const Feed = () => {
   return (
     <div className="space-y-6 pb-24 max-w-lg">
       <Input />
+      {JSON.stringify(realtimePosts)}
       {/* Posts */}
+      {realtimePosts.map((post) => (
+        <>
+          <img src={post.photoUrl} alt="post" />
+          <div>{post.input}</div>
+        </>
+      ))}
     </div>
   );
 };
