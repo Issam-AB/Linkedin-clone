d9e9ec7e9c0f3367476e4a43029e062ee4cbd0a2
Issam <Issam.aboulfadl05@gmail.com>
2022-03-11 23:24:38 +0100

    adding ads section

--- components/Widgets.js
+++ components/Widgets.js
@@ -37,6 +37,17 @@ const Widgets = ({ articles }) => {
         </div>
       </div>
       {/* ADS */}
+      <div className="bg-white dark:bg-[#1D2226] w-11/12 h-64 px-2.5 rounded-lg sticky top-20 border border-gray-300 dark:border-none">
+        <div className="relative w-full h-full">
+          <Image
+            src="https://rb.gy/kbfeaa"
+            layout="fill"
+            objectFit="contain"
+            priority
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 };
